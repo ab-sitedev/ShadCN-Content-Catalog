d9d35114ba60692359c8524830758a70ae1423ad
Aaron Banister <ab.sitedev@gmail.com>
2026-03-08 00:13:24 -0600

    Workaround to netlify's reservation of dynamo SDK env variables

--- netlify/functions/dynamoDB.ts
+++ netlify/functions/dynamoDB.ts
@@ -3,7 +3,11 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION,
+  region: process.env.U_AWS_REGION as string,
+  credentials: {
+    accessKeyId: process.env.U_AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.U_AWS_SECRET_ACCESS_KEY as string,
+  },
 });
 
 const ddb = DynamoDBDocumentClient.from(client);
